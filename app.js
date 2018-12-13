3a1bd3645f88bf5a94055863224e728c5eca42e0
wen <1365525513@qq.com>
2018-12-13 16:18:38 +0800

    node爬虫初始化

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var session = require('express-session'); 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var reptile = require('./routes/reptile');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/reptile', reptile);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
