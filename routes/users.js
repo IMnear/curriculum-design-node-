b1bf932d53f06914efe3b6ab315c21e2fe01298a
wen <1365525513@qq.com>
2018-12-14 15:29:07 +0800

    增加两个登陆接口

--- routes/users.js
+++ routes/users.js
@@ -20,6 +20,106 @@ var responseJSON = function (res, ret) {
   }
 };
 
+// 用户注册
+router.post('/reg', function (req, res, next) {
+  // 从连接池获取连接
+  pool.getConnection(function (err, connection) {
+    // 获取前台页面传过来的参数
+    var param = req.body;
+    var UserName = param.id;
+    var Password = param.name;
+    var _res = res;
+    connection.query(userSQL.queryAll, function (err, res) {
+      var isTrue = false;
+      if (res) { //获取用户列表，循环遍历判断当前用户是否存在
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].id == UserName && res[i].name == Password) {
+            isTrue = true;
+          }
+        }
+      }
+      var data = {};
+      data.isreg = !isTrue; //如果isTrue布尔值为true则登陆成功 有false则失败
+      if (isTrue) {
+        data.result = {
+          code: 1,
+          msg: '用户已存在'
+        }; //登录成功返回用户信息
+      } else {
+        connection.query(userSQL.insert, [param.id, param.name], function (err, result) {
+          if (result) {
+            data.result = {
+              code: 200,
+              msg: '注册成功'
+            };
+          } else {
+            data.result = {
+              code: -1,
+              msg: '注册失败'
+            };
+          }
+        });
+      }
+
+      if (err) data.err = err;
+      // 以json形式，把操作结果返回给前台页面
+      setTimeout(function () {
+        responseJSON(_res, data)
+      }, 300);
+      // responseJSON(_res, data);
+      // 释放链接
+      connection.release();
+
+    });
+  });
+});
+
+
+// 用户登录
+router.post('/login', function (req, res, next) {
+  // 从连接池获取连接
+  pool.getConnection(function (err, connection) {
+    // 获取前台页面传过来的参数
+    var param = req.body;
+    var UserName = param.id;
+    var Password = param.name;
+    var _res = res;
+    console.log(param, '前端传递信息')
+    connection.query(userSQL.queryAll, function (err, res, result) {
+      var isTrue = false;
+      if (res) { //获取用户列表，循环遍历判断当前用户是否存在
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].id == UserName && res[i].name == Password) {
+            isTrue = true;
+          }
+        }
+      }
+      var data = {};
+      data.isLogin = isTrue; //如果isTrue布尔值为true则登陆成功 有false则失败
+      if (isTrue) {
+        data.userInfo = {};
+        data.userInfo.uid = UserName;
+        data.userInfo.userName = Password;
+      } //登录成功返回用户信息
+      if (result) {
+        result = {
+          code: 200,
+          msg: 'succeed'
+        };
+        data.result = result;
+      }
+      if (err) data.err = err;
+      // 以json形式，把操作结果返回给前台页面
+      responseJSON(_res, data);
+
+      // 释放链接
+      connection.release();
+
+    });
+  });
+});
+
+
 
 // 查询用户
 router.get('/get', function (req, res, next) {
@@ -29,7 +129,7 @@ router.get('/get', function (req, res, next) {
     var param = req.query || req.params;
     // 建立连接 增加一个用户信息 
     connection.query(userSQL.queryAll, function (err, result) {
-      console.log(result,'返回结果')
+      console.log(result, '返回结果')
       // 以json形式，把操作结果返回给前台页面     
       responseJSON(res, result);
 
@@ -48,8 +148,8 @@ router.post('/add', function (req, res, next) {
     // 获取前台页面传过来的参数  
     var param = req.body;
     // 建立连接 增加一个用户信息 
-    connection.query(userSQL.insert,[param.id,param.name], function (err, result) {
-      console.log(result,'返回结果')
+    connection.query(userSQL.insert, [param.id, param.name], function (err, result) {
+      console.log(result, '返回结果')
       // 以json形式，把操作结果返回给前台页面     
       responseJSON(res, result);
       // 释放连接  
@@ -65,10 +165,10 @@ router.delete('/delete', function (req, res, next) {
   pool.getConnection(function (err, connection) {
     // 获取前台页面传过来的参数  
     var param = req.query || req.params;
-    console.log('接受到的信息',param)
+    console.log('接受到的信息', param)
     // 建立连接 增加一个用户信息 
-    connection.query(userSQL.deleteUserbyId,[param.id], function (err, result) {
-      console.log(result,'返回结果')
+    connection.query(userSQL.deleteUserbyId, [param.id], function (err, result) {
+      console.log(result, '返回结果')
       // 以json形式，把操作结果返回给前台页面     
       responseJSON(res, result);
       // 释放连接  
