61ac591a9654958f42519979671683b0fdef6930
wen <1365525513@qq.com>
2019-01-04 20:11:46 +0800

    node爬虫修改

--- routes/reptile.js
+++ routes/reptile.js
@@ -6,13 +6,24 @@ var cheerio = require('cheerio');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     console.log('easy node')
-    request('https://www.guahao.com/', function (error, response, body) {
+    request('http://news.baidu.com/', function (error, response, body) {
         if (!error && response.statusCode == 200) {
             //返回的body为抓到的网页的html内容
-            console.log('??')
             var $ = cheerio.load(body); //当前的$符相当于拿到了所有的body里面的选择器
-            var navText = $('.g-container').html(); //拿到导航栏的内容
-            res.send(navText);
+            var arr = [];
+            $(".ulist.focuslistnews").each(function (index, element) { //下面类似于jquery的操作，前端的小伙伴们肯定很熟悉啦
+                var $eleItem = $(element).find('.bold-item a');
+                var $eleItemSon = $(element).find('.bold-item ~ li a')
+                arr.push({
+                    title: $eleItem.text(),
+                    href: $eleItem.attr('href'),
+                    item: {
+                        title: $eleItemSon.text(),
+                        href: $eleItemSon.attr('href')
+                    }
+                });
+            });
+            res.send(arr);
         }
     })
 });
